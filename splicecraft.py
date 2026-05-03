6b1f5d1ca63c538bc44ac46556beb1059f1442ee
ATinyGreenCell <ATinyGreenCell@users.noreply.github.com>
2026-05-03 19:42:27 -0400

    Release v0.5.3.0

--- splicecraft.py
+++ splicecraft.py
@@ -35,7 +35,7 @@ from io import StringIO
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
-__version__ = "0.5.2.0"
+__version__ = "0.5.3.0"
 
 # ── User data directory ────────────────────────────────────────────────────────
 # All user-writable state (library, parts bin, primers, .bak files) lives in
