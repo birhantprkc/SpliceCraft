4e03320eeeeb9d8434012496701269941cc27edf
ATinyGreenCell <ATinyGreenCell@users.noreply.github.com>
2026-05-08 01:09:35 -0400

    Release v0.7.5.0

--- splicecraft.py
+++ splicecraft.py
@@ -39,7 +39,7 @@ from io import StringIO
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
-__version__ = "0.7.4.4"
+__version__ = "0.7.5.0"
 
 # ── Biopython warning filters ─────────────────────────────────────────
 # Some real-world records (notably NCBI Entrez exports that picked up
